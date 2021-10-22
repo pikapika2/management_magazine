e9e8203ec899215343d80bd02728a3664a44a2b0
pikapika2 <yossii0108@gmail.com>
2021-10-22 18:16:32 +0900

    change index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>
-	<h1>hello world</h1>
-    </div>
     <Box>
       <chakra.h1 color="tomato">Hello world</chakra.h1>
     </Box>
