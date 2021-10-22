5e9e3373cb94682040fc871cedef9de57048c084
pikapika2 <yossii0108@gmail.com>
2021-10-22 18:07:09 +0900

    add chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import {AppProps} from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps}/>
+    </ChakraProvider>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { Box, chakra } from '@chakra-ui/react'
+import React from 'react'
+
+export default function HomePage() {
   return (
     <div>
 	<h1>hello world</h1>
     </div>
+    <Box>
+      <chakra.h1 color="tomato">Hello world</chakra.h1>
+    </Box>
   )
 }
