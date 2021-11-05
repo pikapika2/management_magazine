5f2cfe86f49ddb60858d2a1bfc7095bc515436c5
pikapika2 <yossii0108@gmail.com>
2021-11-06 00:11:45 +0900

    change lint
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,38 @@ import React from 'react'
 import Link from 'next/link'
 import 'isomorphic-fetch'
 
-export default function index(props){
+export default function index(props) {
   return (
     <div>
       <h1>表示</h1>
       <ul>
         {props.posts.items.map((item) => {
           //return <li key={post.id}>{post.title}</li>;
-          return <li key={item.id}>{item.selfLink}</li>;
+          return <li key={item.id}>{item.selfLink}</li>
         })}
       </ul>
     </div>
-  );
+  )
 }
  
 //const url = "https://api.github.com/repos/zeit/next.js";
 export async function getServerSideProps() {
   //const url = 'https://jsonplaceholder.typicode.com/posts';
-  const url = encodeURI('https://www.googleapis.com/books/v1/volumes?q=紅葉');
-  const res = await fetch(url);
-  const posts = await res.json();
-  console.log(posts);
+  const url = encodeURI('https://www.googleapis.com/books/v1/volumes?q=紅葉')
+  const res = await fetch(url)
+  const posts = await res.json()
+  console.log(posts)
   //const stars = json.stargazers_count;
   return {
     props: {
       //stars,
       posts: posts,
     },
-  };
+  }
 }
 
-{/*
+{
+  /*
 export default function HomePage() {
   return (
     <Box>
