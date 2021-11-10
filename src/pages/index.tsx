0587b85c808c0713dee1268a01c150700207af02
pikapika2 <yossii0108@gmail.com>
2021-11-10 16:41:49 +0900

    add type
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import { Box, chakra } from '@chakra-ui/react'
 import React from 'react'
 import Link from 'next/link'
-import 'isomorphic-fetch'
+//import 'isomorphic-fetch'
 
 export default function index(props) {
   return (
     <div>
       <h1>表示</h1>
       <ul>
-        {props.posts.items.map((item) => {
+        {props.datas.map((item) => {
           //return <li key={post.id}>{post.title}</li>;
-          return <li key={item.id}>{item.selfLink}</li>
+          return <li key={item.id}>{item.volumeInfo.title} {item.kind}</li>
         })}
       </ul>
     </div>
@@ -19,15 +19,18 @@ export default function index(props) {
 
 export async function getServerSideProps() {
   //const url = 'https://jsonplaceholder.typicode.com/posts';
-  const url = encodeURI('https://www.googleapis.com/books/v1/volumes?q=紅葉')
+  const url = encodeURI('https://www.googleapis.com/books/v1/volumes?q=intitle:恋染紅葉&orderBy=newest')
+  const props
   const res = await fetch(url)
   const posts = await res.json()
-  console.log(posts)
+  const datas = posts.items
+  console.log(datas)
   //const stars = json.stargazers_count;
   return {
     props: {
       //stars,
       posts: posts,
+      datas: datas,
     },
   }
 }
